b9bf95d0627ee7b16ab1080e974ee3b2cc41129d
Tapani Heikonen <tapheiko@gmail.com>
2018-11-09 13:11:30 +0200

    Aloitus commit

---
+++ src/tehtavayksi/TehtavaYksi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tehtavayksi;
+
+/**
+ *
+ * @author User
+ */
+public class TehtavaYksi {
+
+    public static void main(String[] args) {
+        
+        String s_eka="Hello";
+        String s_toka="World";
+        int laskuri=0;
+        while (laskuri<10)
+                {
+                System.out.println(s_eka );
+                System.out.println(s_toka );
+                laskuri++;
+                }
+        
+        
+    }
+}
+    
+
